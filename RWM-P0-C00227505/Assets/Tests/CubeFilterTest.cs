116279ddb1954d53136621a6ad5b27cd1af985e2
MohammadAlhourani <34336016+MohammadAlhourani@users.noreply.github.com>
2020-10-05 09:02:46 +0100

    Tutorial Work
    Tutorial work is completed with assigned filter completed as well

---
+++ RWM-P0-C00227505/Assets/Tests/CubeFilterTest.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+namespace Tests
+{
+    public class CubeFilterTest
+    {
+        [Test]
+        public void CubeFilterSimple()
+        {
+            int[] input = { 1, 3, 5, 7, 9, 999 };
+            int[] output = CubeFilter.cubes(input);
+            int[] expected = { 1, 27, 125, 343, 729, 997002999 };
+
+            CollectionAssert.AreEqual(expected, output);
+        }
+    }
+}
